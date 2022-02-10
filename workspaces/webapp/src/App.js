92cba288a05a4df8a510ecef51301547c0d4dda1
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-02-10 23:40:49 +0200

    clean

--- workspaces/webapp/src/App.js
+++ workspaces/webapp/src/App.js
@@ -1,18 +1,11 @@
-import '@fontsource/roboto/100.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/roboto/900.css';
-import '@fontsource/spartan';
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import theme from './theme';
+import "@fontsource/spartan";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
 
-import Container from './components/Container';
+import Container from "./components/Container";
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Container />
     </ChakraProvider>
   );
