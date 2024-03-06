8858cb9eecacbc030c11d9fd0a56b81348bcd6a8
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-07 01:24:05 +0200

    Update page.tsx

--- apps/web/src/app/frame/page.tsx
+++ apps/web/src/app/frame/page.tsx
@@ -24,30 +24,25 @@ export default async function Home({
   const previousFrame = getPreviousFrame<State>(searchParams);
   const frameMessage = await getFrameMessage(previousFrame.postBody);
 
-  console.log(1)
-
   const { id, imgSrc } = await getImage();
-  console.log(2)
+
   const reducer: FrameReducer<State> = (state, action) => {
-    console.log(3)
-    // if (frameMessage?.buttonIndex == 1 && state.src.length > 0) {
-    //   return {
-    //     src: state.src,
-    //     id: state.id
-    //   };
-    // }
-    // else {
-    return {
-      src: imgSrc,
-      id: id
-    };
-    // }
+    if (frameMessage?.buttonIndex == 1 && state.src.length > 0) {
+      return {
+        src: state.src,
+        id: state.id
+      };
+    }
+    else {
+      return {
+        src: imgSrc,
+        id: id
+      };
+    }
   };
 
-  console.log(4)
   const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
 
-  console.log(5)
   if (!previousFrame.prevState) {
     return (
       <div>
@@ -67,16 +62,12 @@ export default async function Home({
     );
   }
 
-  console.log(6)
   const address = await getAddressForFid({
     fid: frameMessage?.requesterFid ?? 1,
     options: { fallbackToCustodyAddress: true }
   });
 
-  console.log(7)
-
   let alreadyMinted = await hasMinted(address)
-  console.log(8)
 
   if (frameMessage?.buttonIndex == 1) {
     if (previousFrame.prevState.id.length > 0 && frameMessage.recastedCast) {
@@ -84,8 +75,6 @@ export default async function Home({
       alreadyMinted = true;
     }
   }
-
-  console.log(9)
 
   if (alreadyMinted) {
     return (
@@ -109,15 +98,11 @@ export default async function Home({
     );
   }
 
-  console.log(10)
-
   if (previousFrame.prevState.id)
     await unlock(previousFrame.prevState.id);
 
-  console.log(11)
   await lock(state.id)
 
-  console.log(12)
   return (
     <div>
       <FrameContainer
