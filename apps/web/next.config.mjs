b39353a661ff24bc0b79dffc9a75813129ce373b
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-07 01:15:54 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -14,13 +14,7 @@ const nextConfig = {
         protocol: "https",
       },
     ],
-  },
-  webpack: (config, options) => {
-    if (!options.dev) {
-      config.devtool = "source-map";
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
