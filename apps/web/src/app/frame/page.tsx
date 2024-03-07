ded5f718d8925c65d1f50ba558e53e85c6b9aa09
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-07 11:56:18 +0200

    fixes

--- apps/web/src/app/frame/page.tsx
+++ apps/web/src/app/frame/page.tsx
@@ -12,22 +12,21 @@ import { getAddressForFid } from "frames.js"
 import { getImage, hasMinted, lock, mint, unlock } from "./actions";
 
 type State = {
-  src: string
-  id: string
+  id: string | null
+  src: string | null
 };
 
-const initialState: State = { id: "", src: "" };
+const initialState: State = { id: null, src: null };
 
-export default async function Home({
-  searchParams,
-}: NextServerPageProps) {
+export default async function Home({ searchParams }: NextServerPageProps) {
   const previousFrame = getPreviousFrame<State>(searchParams);
   const frameMessage = await getFrameMessage(previousFrame.postBody);
 
   const { id, imgSrc } = await getImage();
 
   const reducer: FrameReducer<State> = (state, action) => {
-    if (frameMessage?.buttonIndex == 1 && state.src.length > 0) {
+    const buttonIndex = action.postBody?.untrustedData.buttonIndex;
+    if (buttonIndex == 1 && state.src != null && state.src.length > 0) {
       return {
         src: state.src,
         id: state.id
@@ -35,30 +34,28 @@ export default async function Home({
     }
     else {
       return {
+        id: id,
         src: imgSrc,
-        id: id
       };
     }
   };
 
   const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
 
-  if (!previousFrame.prevState) {
+  if (state.src == null) {
     return (
-      <div>
-        <FrameContainer
-          pathname="/frame"
-          postUrl="/frame/post"
-          state={state}
-          previousFrame={previousFrame}
-        >
-          <FrameImage src="https://nicepfp.art/assets/welcome.png" aspectRatio="1:1">
-          </FrameImage>
-          <FrameButton>
-            Generate nicepfp
-          </FrameButton>
-        </FrameContainer>
-      </div >
+      <FrameContainer
+        pathname="/frame"
+        postUrl="/frame/post"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage src="https://nicepfp.art/assets/welcome.png" aspectRatio="1:1">
+        </FrameImage>
+        <FrameButton>
+          Generate nicepfp
+        </FrameButton>
+      </FrameContainer>
     );
   }
 
@@ -70,7 +67,7 @@ export default async function Home({
   let alreadyMinted = await hasMinted(address)
 
   if (frameMessage?.buttonIndex == 1) {
-    if (previousFrame.prevState.id.length > 0 && frameMessage.recastedCast) {
+    if (previousFrame.prevState.id != null && previousFrame.prevState.id.length > 0 && frameMessage.recastedCast) {
       await mint(address, previousFrame.prevState.id)
       alreadyMinted = true;
     }
@@ -78,33 +75,32 @@ export default async function Home({
 
   if (alreadyMinted) {
     return (
-      <div>
-        <FrameContainer
-          pathname="/frame"
-          postUrl="/frame/post"
-          state={state}
-          previousFrame={previousFrame}
-        >
-          <FrameImage>
-            <div tw="w-full h-full bg-white text-black justify-center items-center flex">
-              You already minted your nicepfp! ❤️
-            </div>
-          </FrameImage>
-          <FrameButton>
-            Thanks!
-          </FrameButton>
-        </FrameContainer>
-      </div >
+      <FrameContainer
+        pathname="/frame"
+        postUrl="/frame/post"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage>
+          <div tw="w-full h-full bg-white text-black justify-center items-center flex">
+            You already minted your nicepfp! ❤️
+          </div>
+        </FrameImage>
+        <FrameButton>
+          Thanks!
+        </FrameButton>
+      </FrameContainer>
     );
   }
 
   if (previousFrame.prevState.id)
     await unlock(previousFrame.prevState.id);
 
-  await lock(state.id)
+  if (state.id)
+    await lock(state.id)
 
-  return (
-    <div>
+  if (state.src)
+    return (
       <FrameContainer
         pathname="/frame"
         postUrl="/frame/post"
@@ -120,7 +116,22 @@ export default async function Home({
           Redraw
         </FrameButton>
       </FrameContainer>
-    </div >
-  );
+    );
+  else
+    return (
+      <FrameContainer
+        pathname="/frame"
+        postUrl="/frame/post"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage>
+          <div tw="w-full h-full bg-white text-black justify-center items-center flex">
+            Oops, something went wrong 😑
+          </div>
+        </FrameImage>
+
+      </FrameContainer>
+    );
 
 }
