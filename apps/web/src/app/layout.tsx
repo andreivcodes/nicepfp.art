6a6eae08f989407a3f26c87aa82df5f6863ed9fa
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-20 21:08:37 +0200

    plausible

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script defer data-domain="nicepfp.art" src="https://plausible.andreiv.xyz/js/script.js"></script>
+      </Head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
